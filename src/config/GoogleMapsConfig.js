ca5fba42a7f91382e74828306e0c595e6d11623d
Sulton Wibawa <msulton55@gmail.com>
2020-09-25 11:04:33 +0700

    adding Google Maps config for styling

---
+++ src/config/GoogleMapsConfig.js
@@ -0,0 +1,35 @@
+const MapStyle = [
+  {
+    featureType: "administrative",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "poi",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "water",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+const _mapLoaded = (mapProps, map) => {
+  map.setOptions({
+    styles: MapStyle
+  });
+};
+
+export default _mapLoaded;
