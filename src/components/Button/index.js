5d3660e423dd9a52d064ccd7a3e74160f89595ee
Sulton Wibawa <msulton55@gmail.com>
2020-09-28 14:52:21 +0700

    fixing key props from a tag to link tag

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Button = (props) => {
@@ -7,15 +7,15 @@ const Button = (props) => {
       {props.lists.map((list, index) => {
           if (list === "Map")
             return (
-              <Link to="/">
-                <a className="list-group-item list-group-item-action" key={index} onClick={props.option}>
+              <Link to="/" key="index">
+                <a className="list-group-item list-group-item-action" onClick={props.option}>
                   {list}
                 </a>
               </Link>
             )
           else 
             return (
-              <Link to="/about-me">
+              <Link to="/about-me" key={index}>
                 <a className="list-group-item list-group-item-action" key={index} onClick={props.option}>
                   {list}
                 </a>
