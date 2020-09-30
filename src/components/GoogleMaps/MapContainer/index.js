efc03a2bffadb5da43eee2f3901147266edf8246
Sulton Wibawa <msulton55@gmail.com>
2020-10-01 01:11:44 +0700

    integrate with CoordinateContext and DistanceContext

--- src/components/GoogleMaps/MapContainer/index.js
+++ src/components/GoogleMaps/MapContainer/index.js
@@ -2,32 +2,22 @@ import React, { useEffect, useState, useRef } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import MapConfig from '../../../config/GoogleMapsConfig';
 import { Haversin } from '../../../function/Haversin';
+import { SharedCoordinate } from '../../../context/CoordinateContext';
+import { SharedDistance } from '../../../context/DistanceContext';
 
 const MapContainer = (props) => {
-
   const firstUpdate = useRef(true)
   const [sourceCoordinate] = useState({lat: 48.8566, lng: 2.3522})
   const [currentCoordinate, setCurrentCoordinate] = useState({lat: 0, lng: 0})
   const [distance, setDistance] = useState()
 
   useEffect(() => {
-    console.log("MapContainer render")
-    console.log("Source coordinate -> " + JSON.stringify(sourceCoordinate))
-    console.log("Current coordinate -> " + JSON.stringify(currentCoordinate))
-    console.log("Distance from current to source -> " + distance)
-  }, [])
-
-  useEffect(() => {
     if (firstUpdate.current) {
       firstUpdate.current = false
       return
     }
-    console.log("current coordinate -> " + JSON.stringify(currentCoordinate))
-    console.log("Distance from current to source -> " + distance)
-    if (distance < 100)
-      console.log("You got 1 point")
-    else
-      console.log("No point")
+    SharedCoordinate(currentCoordinate)
+    SharedDistance(distance)
   }, [distance])
 
   const mapClickHandler = (x, y, z) => {
@@ -35,7 +25,6 @@ const MapContainer = (props) => {
     let currentLat = z.latLng.lat()
     let currentLng = z.latLng.lng()  
 
-    console.clear()
     setCurrentCoordinate({lat: currentLat, lng: currentLng})
     tempDistance =  Haversin(currentLat, currentLng, sourceCoordinate.lat, sourceCoordinate.lng)
     setDistance(tempDistance)
@@ -51,10 +40,9 @@ const MapContainer = (props) => {
         initialCenter={{lat: 54.5260, lng: 15.2551}}
         onClick={mapClickHandler}
         onReady={MapConfig}>
-
-      <Marker position={sourceCoordinate} />
+      
+      {/* <Marker position={sourceCoordinate} /> */}
       {currentCoordinate !== undefined ? <Marker position={currentCoordinate} onClick={markerClickHandler} /> : <div />}
-
     </Map>
   )
 }
